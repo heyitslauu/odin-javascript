ef51e77b43964207ff9f92fc194645c0b4447aa1
heyitslauu <laurencecadag1@gmail.com>
2023-08-30 23:27:38 +0800

    v1.0 Finished

--- projects/library-app/app.js
+++ projects/library-app/app.js
@@ -10,9 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
     renderBooks();
     const copyRight = document.createElement('p');
     let date =  new Date().getFullYear();
-    copyRight.textContent =  `Copyright © ${date} heyitslauu`
+    copyRight.innerHTML =  `Copyright © ${date} <a href="https://github.com/heyitslauu" class="gh-link">heyitslauu</a>`
     footer.prepend(copyRight)
-
 })
 
 
@@ -50,47 +49,72 @@ function renderBooks() {
 
     myLibrary.forEach((book, index) => {
         placeholder += `
-        <div class="book-card">
-                <div class="book-card__description">
-                    <p class="book_title">${book.title}</p>
-                    <div class="author-pages">
-                        <p class="book_author">${book.author}</p>
-                        <span>|</span>
-                        <span>${book.pages} Pages</span>
-                    </div>
-                </div>
-                <button class="card-btn btn-read">Mark as Read</button>
-                <button class="card-btn btn-delete" data-index="${index}">Remove</button>
+        <div class="book-card ${book.hasRead ? 'read' : ''}">
+            <div class="book-card__description">
+                <h3 class="book_title">${book.title}</h3>
             </div>
+            <h4 class="book_author">By: ${book.author}</h4>
+            <p class="book_pages">${book.pages} pages</p>
+            <div class="marked-read">
+                <label class="has-read-label">
+                    Read
+                    <input type="checkbox" class="card-checkbox" data-index="${index}" ${book.hasRead ? 'checked' : ''}>
+                </label>
+            </div>
+            <button class="card-btn btn-delete" data-index="${index}">Remove</button>
+        </div>
         `
     })
     mainContent.innerHTML = placeholder;
 
     const deleteButtons = document.querySelectorAll('.btn-delete');
+    const readButtons = document.querySelectorAll('.btn-read');
+    const cardCheckBox = document.querySelectorAll('.card-checkbox');
+
     deleteButtons.forEach(button => {
         button.addEventListener('click', handleDelete);
     });
+
+    readButtons.forEach(button => {
+        button.addEventListener('click', handleRead);
+    })
+
+    cardCheckBox.forEach(box => {
+        box.addEventListener('click', handleRead)
+    })
 }
+
+
+function handleRead(event) {
+    const index = event.target.getAttribute('data-index');
+    const card = event.target.closest('.book-card');
+    myLibrary[index].hasRead = !myLibrary[index].hasRead;
+
+    if (myLibrary[index].hasRead) {
+        card.classList.add('read'); 
+    } else {
+        card.classList.remove('read'); 
+    }
+    
+}
+
 
 function handleDelete(event) {
     const index = event.target.getAttribute('data-index');
-    console.log(index);
-    myLibrary.splice(index, 1); // Remove the book from the library
-    renderBooks(); // Re-render the updated library
+    myLibrary.splice(index, 1);
+    renderBooks(); 
 }
 
 
-function Book(title, author, pages, hasRead) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.hasRead = hasRead;
-    this.info = function() {
-        console.log(`The Book is ${title}, published by ${author} with ${pages} pages`);
+class Book {
+    constructor(title, author, pages, hasRead) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.hasRead = hasRead;
     }
 }
 
 function addToLibrary(Book) {
     myLibrary.push(Book)
-    // console.log(myLibrary);
 }
